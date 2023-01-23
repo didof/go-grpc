12b47cf299a2a0b1278ed348632894c2054bca81
Francesco Di Donato <didonato.fr@gmail.com>
2023-01-23 21:30:01 +0100

    update: refactor folder names
    Signed-off-by: Francesco Di Donato <didonato.fr@gmail.com>

---
+++ client/usermgmt_client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	pb "github.com/didof/go-grpc/proto/go_usermgmt_grpc"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewUserManagementClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var new_users = make(map[string]int32)
+	new_users["Alice"] = 43
+	// new_users["Bob"] = 30
+
+	for name, age := range new_users {
+		r, err := client.CreateNewUser(ctx, &pb.NewUser{
+			Name: name,
+			Age:  age,
+		})
+		if err != nil {
+			log.Fatalf("could not create user: %v", err)
+		}
+		log.Printf(`User Details:
+NAME: %s
+AGE: %d
+ID: %d`, r.GetName(), r.GetAge(), r.GetId())
+	}
+
+	params := &pb.GetUserParams{}
+	r, err := client.GetUsers(ctx, params)
+	if err != nil {
+		log.Fatalf("could not retrieve users: %v", err)
+	}
+	fmt.Println(r)
+}
